a2623a63954c17539405e294e3b4194f9fae2fd3
kevit-lav-ughareja <lav.ughareja@botprosolutions.com>
2025-06-23 13:00:11 +0530

    install socket.io pack and fix normal error

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,11 +1,15 @@
-const http = require("https");
+const http = require("http");
 const express = require("express");
 const path = require("path");
 
 const app = express();
-
+app.use(express.static(path.resolve("./public")));
 const server = http.createServer(app);
 
-app.listen(9000, () => {
+app.get("/", (req, res) => {
+  return res.sendFile(path.resolve("./public/index.html"));
+});
+
+server.listen(9000, () => {
   console.log(`Server started at PORT:9000`);
 });
